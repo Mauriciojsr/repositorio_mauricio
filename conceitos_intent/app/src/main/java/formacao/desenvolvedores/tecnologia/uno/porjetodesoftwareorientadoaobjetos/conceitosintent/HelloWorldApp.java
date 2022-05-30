91a0c039dbd8d38820c39244f884b7a5a8c8ea48
Mauricio Jose da Silva Ramos <mauriciojsr@hotmail.com>
2022-05-30 11:13:11 -0300

    Delete HelloWorldApp.java
    Deletado classe hello world!

--- conceitos_intent/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/porjetodesoftwareorientadoaobjetos/conceitosintent/HelloWorldApp.java
+++
@@ -1,7 +0,0 @@
-package formacao.desenvolvedores.tecnologia.uno.porjetodesoftwareorientadoaobjetos.conceitosintent;
-
-public class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
