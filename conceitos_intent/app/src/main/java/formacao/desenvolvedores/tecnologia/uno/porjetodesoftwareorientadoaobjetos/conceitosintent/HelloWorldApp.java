3bb55def955735697f1cfec7403259dd9ecff1ab
Mauricio Jose da Silva Ramos <mauriciojsr@hotmail.com>
2022-05-30 10:58:41 -0300

    Aula 30/05/2022
    Colocado projeto no git

---
+++ conceitos_intent/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/porjetodesoftwareorientadoaobjetos/conceitosintent/HelloWorldApp.java
@@ -0,0 +1,7 @@
+package formacao.desenvolvedores.tecnologia.uno.porjetodesoftwareorientadoaobjetos.conceitosintent;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
