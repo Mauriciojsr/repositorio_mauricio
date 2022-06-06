03fb0536f966a63f5ed6513615b71a63768a044b
Mauricio Jose da Silva Ramos <mauriciojsr@hotmail.com>
2022-06-06 18:37:52 -0300

    Alterações extra classe
    06/06/2022

--- calcular/app/src/main/java/ramos/s/j/mauricio/formacao/desenvolvedores/tecnologia/uno/calcular/MainActivity.java
+++ calcular/app/src/main/java/ramos/s/j/mauricio/formacao/desenvolvedores/tecnologia/uno/calcular/MainActivity.java
@@ -3,11 +3,14 @@ package ramos.s.j.mauricio.formacao.desenvolvedores.tecnologia.uno.calcular;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,10 +22,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public static final String MULTIPLICAR = "multiplicar";
     public static final String SOMAR       = "somar";
     public static final String SUBTRAIR    = "subtrair";
-    private TextView tvOpcao, tvOperacao, tvResultado;
+    private TextView tvResultado;
     private Spinner spiOpcoes;
     private EditText edtNumero1, edtNumero2;
-    private ImageView imgOperacao;
+    private ImageView imgSoma;
     private Button btnCalular;
 
     @Override
@@ -34,25 +37,31 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spiOpcoes   = findViewById(R.id.spiOpcoes);
         edtNumero1  = findViewById(R.id.edtNumero1);
         edtNumero2  = findViewById(R.id.edtNumero2);
-        imgOperacao = findViewById(R.id.imgOperacao);
+        imgSoma     = findViewById(R.id.imgSoma);
         btnCalular  = findViewById(R.id.btnCalcular);
 
-        imgOperacao.setVisibility(View.INVISIBLE);
+        imgSoma.setVisibility(View.INVISIBLE);
 
 
-        ArrayAdapter<String> adapterOperacoesMatematicas = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,
-                getResources().getStringArray(R.array.operacoes_matematicas));
-        adapterOperacoesMatematicas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapterOperacoes = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item
+                , getResources().getStringArray(R.array.operacoes_matematicas));
+        adapterOperacoes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-
-        spiOpcoes.setAdapter(adapterOperacoesMatematicas);
-        spiOpcoes.setOnItemClickListener((AdapterView.OnItemClickListener) this);//Usada como argumento do método.
+        spiOpcoes.setAdapter(adapterOperacoes);
+        spiOpcoes.setOnItemSelectedListener(this);//Usada como argumento do método.
 
         String operacaoSelecionada = spiOpcoes.getSelectedItem().toString();
 
         btnCalular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                tvResultado.setText(String.valueOf(DIVIDIR));
+                tvResultado.setText(String.valueOf(MULTIPLICAR));
+                tvResultado.setText(String.valueOf(SOMAR));
+                tvResultado.setText(String.valueOf(SUBTRAIR));
+
 
                 if(operacaoSelecionada == DIVIDIR){
 
@@ -77,12 +86,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        Toast.makeText(this, "Teste",  Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i).toString()
+                , Toast.LENGTH_SHORT).show();
+
+        if(adapterView.getItemAtPosition(i).toString() == DIVIDIR){
+
+        }else if(adapterView.getItemAtPosition(i).toString().equals(MULTIPLICAR)){
+
+        }else if(adapterView.getItemAtPosition(i).toString().equals(SOMAR)){
+            imgSoma.setVisibility(View.VISIBLE);
+
+        }else if(adapterView.getItemAtPosition(i).toString().equals(SUBTRAIR)){
+
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-        Toast.makeText(this, "Teste 2", Toast.LENGTH_SHORT).show();
+
     }
 
 
